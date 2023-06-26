67a2812791ffb86327a3338a3addeca2633e9270
Jaison5008 <99020825+Jaison5008@users.noreply.github.com>
2023-06-26 20:15:32 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -116,14 +116,14 @@ import { toast } from 'react-toastify';
 <ul className='pagination'> 
 
 <li className='page-item'>  
-<a  href='#' className='page-link'onClick={prePage}>prev</a>
+<p className='page-link'onClick={prePage}>prev</p>
 </li>
 { Number.map((n,i)=>(
 <li className={`page-item ${currentPage=== n?'active':''}`} key={i}> 
-<a href='#' className='page-link'onClick={()=>curPage(n)}>{n}</a>
+<p className='page-link'onClick={()=>curPage(n)}>{n}</p>
 </li>  ))  }
 <li> 
-<a href='#'  className='page-link'onClick={nxtPage}>next</a>
+<p className='page-link'onClick={nxtPage}>next</p>
 </li>
 
 
