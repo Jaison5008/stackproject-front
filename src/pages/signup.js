6cc18a7e613809150ffa1aefe64e8c92cb0d3f9c
Jaison5008 <99020825+Jaison5008@users.noreply.github.com>
2023-06-26 16:00:57 +0530

    Update signup.js

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -5,7 +5,7 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { toast } from 'react-toastify'; 
 import axios from "axios";
- const url="http://localhost:5000/"; 
+ const url="https://stack-back-qu4a.onrender.com";
  function Signup() { 
     const navi=useNavigate()
     const [email,setEmail]=useState(''); 
